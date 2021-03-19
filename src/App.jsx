816b4453d51806a0d326ed0e511c4db98dbe35d2
judithmg <jmartigo33@gmail.com>
2021-03-19 21:26:42 +0100

    Modified structure Removed router Now rendering all on app.jsx Removed redux files

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
 import './styles/scss/App.scss';
-import React from 'react';
-import {
-  Route, Switch, BrowserRouter as Router,
-} from 'react-router-dom';
+import './styles/scss/PressStart.scss';
+import React, { useState } from 'react';
+
 import Main from './pages/Main';
-import PressStart from './pages/PressStart';
 
 function App() {
+  const [init, setInit] = useState(true);
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/welcome">
-          <Main />
-        </Route>
+    <>
+      {
+      init ? (
+        <section className="start__container" data-aos="fade-in">
 
-        <Route path="/">
-          <PressStart />
-        </Route>
-      </Switch>
-    </Router>
+          <button
+            type="button"
+            className="nes-btn is-warning btn__press-start words"
+            onClick={() => setInit(!init)}
+          >
+            <span className="word">P</span>
+            <span className="word">R</span>
+            <span className="word">E</span>
+            <span className="word">S</span>
+            <span className="word">S</span>
+            <span className="word">&nbsp;</span>
+            <span className="word">S</span>
+            <span className="word">T</span>
+            <span className="word">A</span>
+            <span className="word">R</span>
+            <span className="word">T</span>
 
+          </button>
+
+        </section>
+      ) : (<Main />)
+    }
+
+    </>
   );
 }
 
