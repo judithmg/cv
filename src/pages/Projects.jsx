36c12dcf25e003e7e15313f3f5ead7413ee02004
judithmg <jmartigo33@gmail.com>
2021-04-06 22:06:11 +0200

    Changes to data Fixed problem with imgs

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,20 +6,17 @@ import ProjectInfo from './ProjectInfo';
 
 export default function Projects() {
   return (
-    <>
-
-      <section className="nes-container with-title main-box projects__container">
-        <p className="title">Projects</p>
-        <div className="projects__previews">
-          {
+    <section className="nes-container with-title main-box projects__container">
+      <p className="title">Projects</p>
+      <div className="projects__previews">
+        {
             projectData.map((project) => (
               <>
                 <ProjectInfo project={project} key={project.title} />
               </>
             ))
 }
-        </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 }
