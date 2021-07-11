3c7d3eb3d5f92b4d5a8db05d2c2b60efe4ba6983
judithmg <jmartigo33@gmail.com>
2021-07-11 16:12:37 +0200

    Added HelloWorld constants

---
+++ src/constants/helloWorld.js
@@ -0,0 +1,5 @@
+export default [
+  'Hello, world.',
+  'My name is Judith Martínez and I am a Biologist who decidied to take a chance at Web Developing. ',
+  'Nowadays I am a fullstack developer with a code based in SOLID principles and TDD. ',
+  'In this portfolio you can learn more about the technologies I have worked with and the projects I am working on.'];
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,9 @@
 import backend from './backend';
 import frontend from './frontend';
 import currently from './currently';
+import helloWorld from './helloWorld';
 import projectsData from './projects-data';
 
 export {
-  backend, frontend, currently, projectsData,
+  backend, frontend, currently, projectsData, helloWorld,
 };
