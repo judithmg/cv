836d168114e288c628ec48517a04f3072fe5aeb7
judithmg <jmartigo33@gmail.com>
2021-07-11 13:04:20 +0200

    Created constants folder

---
+++ src/constants/projects-data.js
@@ -0,0 +1,94 @@
+const projectsData = [
+  {
+    title: 'PASAPALABRA',
+    technologies: 'HTML, CSS, JS',
+    info:
+      'Inspired Star Wars Pasapalabra game build during the Skylab Coders Academy precourse',
+    previewLink: '/images/pas.png',
+    modalImg: 'https://i.imgur.com/JKRoEU2.png',
+  },
+  {
+    title: 'CALCULATOR',
+    technologies: 'HTML, CSS, JS',
+    info: 'Simple calculator build during the Skylab Coders Academy precourse',
+    previewLink: '/images/calc.png',
+    modalImg: 'https://i.imgur.com/2m6DLG5.png',
+  },
+  {
+    title: 'RAKUTEN TV',
+    technologies: 'HTML, CSS, JS',
+    info:
+      "Rakuten's dashboard an about page for a single TV show mock, build during my first week at Skylab Coders Academy",
+    demoLink: 'https://judith-rakuten.netlify.app/',
+    codeLink: 'https://github.com/judithmg/rakuten',
+    previewLink: '/images/rak.png',
+    modalImg: 'https://i.imgur.com/ZtsCVYB.png',
+  },
+  {
+    title: 'GAME OF LIFE',
+    technologies: 'HTML, CSS, JS',
+    info:
+      'The Game of Life is a cellular automaton devised by the British mathematician John Horton Conway in 1970. This Javascript implementation follows its rules, allows players to draw their patterns or to create random ones.',
+    demoLink: 'https://judith-gameoflife.netlify.app/',
+    codeLink: 'https://github.com/judithmg/game-of-life',
+    previewLink: '/images/gol.png',
+    modalImg: 'https://i.imgur.com/4YUCRjo.png',
+  },
+  {
+    title: 'ANIME PICKER',
+    technologies: 'HTML, CSS, JS',
+    info:
+      'To put into practice fetching data from public APIs and the use of Javascript Promises, this app that suggests users different animes based on their input was developed.',
+    demoLink: 'https://judith-animepicker.netlify.app/',
+    codeLink: 'https://github.com/judithmg/anime-picker',
+    previewLink: '/images/ani.png',
+    modalImg: 'https://i.imgur.com/u9ZxrjW.png',
+  },
+  {
+    title: 'POKÉDEX',
+    technologies: 'HTML, CSS, JS',
+    info: 'Pokédex using PokéApi as source for data.',
+    demoLink: 'https://judith-pokelista.netlify.app/',
+    codeLink: 'https://github.com/judithmg/html-pokedex',
+    previewLink: '/images/poke.png',
+    modalImg: 'https://i.imgur.com/8wwJu00.png',
+  },
+  {
+    title: 'SOCIO.LEGAL',
+    technologies: 'React, Redux, Mongoose, Postman',
+    info:
+      'Group project build at Skylab Coders with my classmates Nil Rivera, Álvaro Valladares and Xi Zhou. Socio Legal allows the user to manage their data on different transactions.',
+    previewLink: '/images/socio.png',
+    modalImg: 'https://i.imgur.com/44Mialy.png',
+  },
+  {
+    title: 'THIS PORTFOLIO',
+    technologies: 'React, Sass',
+    info: 'Portfolio build to showcase my abilities.',
+    demoLink: 'https://judith-cv.netlify.app',
+    codeLink: 'https://github.com/judithmg/cv',
+    previewLink: '/images/cv.png',
+    modalImg: 'https://i.imgur.com/PPovVFj.png',
+  },
+  {
+    title: 'PHASER GAME',
+    technologies: 'HTML, Phaser, TypeScript',
+    info:
+      'Phaser game made for fun. There was an inner joke behind it, you might not get it (sorry!)',
+    codeLink: 'https://github.com/judithmg/phaserTalk',
+    previewLink: '/images/phaser.png',
+    modalImg: '/images/phas2.png',
+  },
+  {
+    title: 'ONE PIECE TREASURE CRUISE DATABASE',
+    technologies: 'TypeScript, React, SCSSS, Node.Js, MongoDb',
+    info:
+      'Database for the game One Piece Treasure Cruise with info about all the available characters. Allows users to decide wether to use a dark or a light theme. Filtering options allow users to be more specific on their search.',
+    codeLink: 'https://github.com/judithmg/one-piece-ts',
+    demoLink: 'https://optc-judith.netlify.app/',
+    previewLink: '/images/optc.png',
+    modalImg: '/images/optc-bg.png',
+  },
+];
+
+export default projectsData;
