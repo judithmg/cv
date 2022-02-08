2e4e8edbd49ad21b81749434573b92b785cb3fd0
judithmg <jmartigo33@gmail.com>
2022-02-08 17:58:03 +0100

    delete files

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,9 @@
-import backend from './backend';
-import frontend from './frontend';
 import currently from './currently';
-import footer from './footer';
 import education from './education';
+import footer from './footer';
 import helloWorld from './helloWorld';
 import projectsData from './projects-data';
 
 export {
-  backend, frontend, currently, projectsData, helloWorld, footer, education,
+  currently, projectsData, helloWorld, footer, education,
 };
